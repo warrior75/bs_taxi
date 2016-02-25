9023685d3daa174cc8bc4ea7719157ae2bd9e693
warrior75 <waryor75@gmail.com>
2016-02-25 18:12:00 +0100

    add Session

--- app/Manager/SessionManager.php
+++ app/Manager/SessionManager.php
@@ -8,7 +8,7 @@ class SessionManager extends \W\Manager\Manager
 			return false;
 		}
 
-		$sql = "SELECT * FROM " . $this->table . " WHERE user_id = :id LIMIT 1";
+		$sql = "SELECT * FROM " . $this->table . " WHERE users_id = :id LIMIT 1";
 		$sth = $this->dbh->prepare($sql);
 		$sth->bindValue(":id", $id);
 		$sth->execute();
