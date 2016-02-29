4382180ae187100e78ebb14170a81f034a3341d0
warrior75 <waryor75@gmail.com>
2016-02-29 14:36:56 +0100

    fix formateur

--- app/templates/formateur/index.php
+++ app/templates/formateur/index.php
@@ -41,8 +41,8 @@
                     <td>
                         <div class="progress progress-striped active">
                         <?php $progress = ($summaryCours[$etudiant['id']]['nbCoursValide'] * 100)/$nbCoursTotal; ?>
-                        <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="<?= $progress; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?= $progress; ?>%">
-                        <span class="sr-only">  </span>
+                        <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="<?= $progress; ?>" aria-valuemin="0" aria-	valuemax="100" style="width: <?= $progress; ?>%">
+                        <?php echo round($progress);?>%
                         </div>
                         </div> 
                     </td>   
